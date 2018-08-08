f2c6e8a2cda59b1bcacef135c3f3921dc0816a2a
samarth saxena <samarth19sam@gmail.com>
2018-08-08 23:58:47 +0530

    itertools Product function

---
+++ itertoolsProduct.py
@@ -0,0 +1,18 @@
+# itertools.product()
+# This tool compute the cartesian product of input tables
+#Ex- product(a,b) is same as ((x,y) for x in A for y in B)
+from itertools import product
+
+print (list(product([1,2,3],repeat = 2)))
+
+print( list(product([1,2,3],[3,4])))
+
+A=[[1,2,3] , [3,4,5]]
+print(list(product(*A)))
+
+B = [[1,2,3],[3,4,5],[7,8]]
+print(list(product(*B)))
+
+
+# Taking Input from list
+a = list(map(int, input().split()))
