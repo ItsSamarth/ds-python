5a31fd286945034275720f50282053b69ac5dfad
samarth <samarth19sam@gmail.com>
2018-08-20 18:27:57 +0530

    hackerearth set 2 Noor and pond

--- competitions/heNoorAndHisPond.py
+++ competitions/heNoorAndHisPond.py
@@ -1,12 +1,36 @@
-tc= int(input())
-n= int(input())
-size=[]
-ef=[]
+# tc= int(input())
+# n= int(input())
+# size=[]
+# ef=[]
+# fish=[]
+# for i in range(tc):
+#     for j in range(n):
+#         a,b = map(int, input().split())
+#         size.append(a,b)
+#         ef.append(b)
+#     for k in range(n):
+#         for l in range(n):
+#             if(size[j]<=ef[i]):
+#                 size.remove(size[j])
+#                 ef
+#         if(size[i]<ef[i]):
+#
+#     print(size)
+#     print(ef)
+
+tc = int(input())
+n = int(input())
 fish=[]
-for i in range(tc):
-    for j in range(n):
-        a,b = map(int, input().split())
-        size.append(a,b)
-        ef.append(b)
-    print(size)
-    print(ef)
+index=[]
+for i in range(n):
+    fish.append(input().split())
+fish = sorted(fish)
+for i in range(len(fish)):
+    for j in range(len(fish)):
+        if(fish[i][1] >= fish[j][0] and i !=j):
+            index.append(j)   #ef>= size
+
+for i in index:
+    fish.remove(fish[i])
+
+print(len(fish))
--- competitions/test.py
+++ competitions/test.py
@@ -1,25 +1,9 @@
 tc = int(input())
-
-for i in range(tc):
-    sum=0
-    count=0
-    months=0
-    d,a,m,b,x = map(int, input().split())
-    while(d<x):
-        # print("coming inside")
-        rem = (x-d)//a
-        if(rem > m):
-            print("1st section")
-            d=d+a*m+b
-            months +=m+1
-        else:
-            print("2nd sec")
-            if rem==0:
-                d=d+a*1
-                months += 1
-            else:
-                d=d+a*rem
-                months += rem
-        print(d)
-        print("months",m)
-    print(months)
+n = int(input())
+fish=[]
+for i in range(n):
+    fish.append(input().split())
+fish = sorted(fish)
+while(print(len(fish))):
+    if(len(list) != 0):
+        fish.remove(fish[1])
