ab7e7c08308dc8727763aadbb62baecf9322ae5e
samarth <samarth19sam@gmail.com>
2018-09-11 15:45:36 +0530

    naive vs russian peasant algorithm

---
+++ udacity/russian_peasant_algorithm.py
@@ -0,0 +1,19 @@
+#RUSSIAN PEASANTS ALGORITHM
+
+def russian(a,b):
+    x=a
+    y=b
+    z=0
+    count = 0
+    while x> 0:
+        if x % 2 == 1:
+             z = z+y
+             count +=1
+        y = y<<1
+        x = x>>1
+
+    print(count)
+    return z
+
+# output = 14*11
+print(russian(20,7))
