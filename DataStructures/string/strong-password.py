a9c7061bb5b945b341262e9407935c9f88a45e3b
samarth saxena <samarth19sam@gmail.com>
2018-10-04 23:02:53 +0530

    fix

--- DataStructures/string/strong-password.py
+++ DataStructures/string/strong-password.py
@@ -3,11 +3,11 @@ def minimumNumber(n, password):
     count = 0
     if any(i.isdigit() for i in password) == False:
         count+=1
-    elif any(i.islower() for i in password) == False:
+    if any(i.islower() for i in password) == False:
         count+=1
-    elif any(i.isupper() for i in password) ==False:
+    if any(i.isupper() for i in password) ==False:
         count+=1
-    elif any(i in '!@#$%^&*()-+' for i in password) == False:
+    if any(i in '!@#$%^&*()-+' for i in password) == False:
         count+=1
 
     return max(count, 6-n)
