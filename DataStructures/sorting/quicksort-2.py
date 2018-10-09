e765e43a70a415e76e42e225da82672acf7a2e0e
samarth <samarth19sam@gmail.com>
2018-10-09 15:22:28 +0530

    Fix - quicksort

--- DataStructures/sorting/quicksort-2.py
+++ DataStructures/sorting/quicksort-2.py
@@ -1,8 +1,26 @@
 # https://www.hackerrank.com/challenges/quicksort2/problem
-def quick_sort(ar):
-    return ""
+def partition(arr):
+    pivot = arr[0]
+    low=[]
+    high=[]
+    for i in range (1,len(arr)):
+        if arr[i] < pivot:
+            low.append(arr[i])
+        elif arr[i] > pivot:
+            high.append(arr[i])
+    return low,pivot,high
+
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    low, pivot, high = partition(arr)
+    # print(low)
+    # print(high)
+    print(low+ [pivot]+high)
+    return quick_sort(low)+[pivot]+quick_sort(high)
 
 
 m = 7
 ar = [5, 8, 1, 3, 7, 9, 2]
-quick_sort(ar)
+print(quick_sort(ar))
